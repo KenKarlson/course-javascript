1fd9c4572c15df2624dec36a146ebf452980ace1
Ken Karlson <kenkarlson@mail.ru>
2022-05-04 13:07:45 +0300

    add all tasks

--- projects/functions/index.js
+++ projects/functions/index.js
@@ -1,50 +1,59 @@
+'use strict';
 /* ДЗ 1 - Функции */
-
 /*
- Задание 1:
+  Задание 1:
 
- 1.1: Добавьте к функции параметр с любым именем
- 1.2: Функция должна возвращать аргумент, переданный ей в качестве параметра
+1.1: Добавьте к функции параметр с любым именем
+1.2: Функция должна возвращать аргумент, переданный ей в качестве параметра
 
- Пример:
-   returnFirstArgument(10) вернет 10
-   returnFirstArgument('привет') вернет `привет`
+  Пример:
+    returnFirstArgument(10) вернет 10
+    returnFirstArgument('привет') вернет `привет`
 
- Другими словами: функция должна возвращать в неизменном виде то, что поступает ей на вход
+  Другими словами: функция должна возвращать в неизменном виде то, что поступает ей на вход
  */
-function returnFirstArgument(value) {}
+function returnFirstArgument(value) {
+  //my code
+  return value;
+}
 
 /*
- Задание 2:
+  Задание 2:
 
- 2.1: Функция должна возвращать сумму переданных аргументов
+2.1: Функция должна возвращать сумму переданных аргументов
 
- Пример:
-   sumWithDefaults(10, 20) вернет 30
-   sumWithDefaults(2, 4) вернет 6
+Пример:
+  sumWithDefaults(10, 20) вернет 30
+  sumWithDefaults(2, 4) вернет 6
 
  2.2 *: Значение по умолчанию для второго аргумента должно быть равно 100
 
- Пример:
-   sumWithDefaults(10) вернет 110
+Пример:
+  sumWithDefaults(10) вернет 110
  */
-function sumWithDefaults(a, b) {}
+function sumWithDefaults(a, b = 100) {
+  //my code
+  return a + b;
+}
 
 /*
- Задание 3:
+Задание 3:
 
- Функция должна принимать другую функцию и возвращать результат вызова этой функции
+Функция должна принимать другую функцию и возвращать результат вызова этой функции
 
- Пример:
-   returnFnResult(() => 'привет') вернет 'привет'
+Пример:
+  returnFnResult(() => 'привет') вернет 'привет'
  */
-function returnFnResult(fn) {}
+function returnFnResult(fn) {
+  //my code
+  return fn();
+}
 
 /*
- Задание 4:
+Задание 4:
 
- Функция должна принимать число (если ничего не передано, то воспринимать как 0) и возвращать новую функцию (F)
- При вызове функции F, переданное ранее число должно быть увеличено на единицу и возвращено из F
+  Функция должна принимать число (если ничего не передано, то воспринимать как 0) и возвращать новую функцию (F)
+  При вызове функции F, переданное ранее число должно быть увеличено на единицу и возвращено из F
 
  Пример:
    var f = returnCounter();
@@ -56,7 +65,9 @@ function returnFnResult(fn) {}
    console.log(f()); // выведет 12
    console.log(f()); // выведет 13
  */
-function returnCounter(number) {}
+function returnCounter(number = 0) {
+  return () => ++number;
+}
 
 /*
  Задание 5 *:
@@ -67,7 +78,9 @@ function returnCounter(number) {}
  Пример:
    returnArgumentsArray(1, 2, 3) вернет [1, 2, 3]
  */
-function returnArgumentsArray() {}
+function returnArgumentsArray(...args) {
+  return args;
+}
 
 /*
  Задание 6 *:
@@ -84,7 +97,10 @@ function returnArgumentsArray() {}
 
    console.log(newSum()) выведет 6
  */
-function bindFunction(fn, ...args) {}
+function bindFunction(fn, ...args) {
+  //my code
+  return () => fetch(...args);
+}
 
 export {
   returnFirstArgument,
